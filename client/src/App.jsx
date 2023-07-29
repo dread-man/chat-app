543a1189c84ca5489aef945a00a069ec805b7971
David <davidsokol112@gmail.com>
2023-07-29 04:38:27 +0300

    stage 1.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,6 @@ function App() {
         }
     }
 
-	
-
     return (
         <div className="App">
             {!showChat ? (
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import ScrollToBottom from 'react-scroll-to-bottom'
 
